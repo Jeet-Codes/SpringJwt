6d1b41f7bd184dbe2d18c0f0a1800a03d67b5e69
Sandeep Kumar <sandeepkumar090304@gmail.com>
2024-01-20 22:04:42 +0530

    This is Example of Spring Security

---
+++ src/main/java/com/jwt/jwtexample/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.jwt.jwtexample.controller;
+
+import com.jwt.jwtexample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user")
+    public List getuser(){
+        System.out.println("Getting users");
+        return this.userService.getUsers();
+    }
+}
